e92b99cf1d5d434ebb9dc2d14d2d79a26efcafd6
BarRaider <46548278+BarRaider@users.noreply.github.com>
2019-10-01 20:59:25 -0700

    v1.3 - Video Player + Twitch Integration

--- streamdeck-obstools/Twitch/TwitchSecret.cs
+++ streamdeck-obstools/Twitch/TwitchSecret.cs
@@ -6,6 +6,6 @@ namespace ChatPager.Twitch
 {
     internal static class TwitchSecret
     {
-        internal static readonly string CLIENT_ID = "aao2j8zfthy2rh10satdmxwojt70ph";
+        internal static readonly string CLIENT_ID = "";
     }
 }
